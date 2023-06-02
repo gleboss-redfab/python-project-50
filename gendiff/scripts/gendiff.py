cec3235f61fb5f85a278f58c4bc03847a8171816
gleboss-redfab <gleb.v@redfab.ru>
2023-06-02 18:03:05 +0700

    implement compare algo and refactor

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,20 +1,51 @@
 import argparse
+import json
 
 
-def main():
+def init_cli_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
     parser.add_argument('-f', '--format', dest='format',
                         help='set format of output')
-    # parser.add_argument('--sum', dest='accumulate', action='store_const',
-    #                     const=sum, default=max,
-    #                     help='sum the integers (default: find the max)')
 
+    return parser
+
+
+def generate_diff(path1, path2):
+    # read data
+    json1 = json.load(open(path1))
+    json2 = json.load(open(path2))
+
+    # get unique keys and sort it
+    keys = list(set(json1.keys()) | set(json2.keys()))
+    keys.sort()
+
+    # compare and create response
+    response = []
+    for key in keys:
+        if key in json1.keys() and key not in json2.keys():
+            response.append(f"- {key}: {json1[key]}")
+        elif key in json2.keys() and key not in json1.keys():
+            response.append(f"+ {key}: {json2[key]}")
+        elif json1[key] != json2[key]:
+            response.append(f"- {key}: {json1[key]}")
+            response.append(f"+ {key}: {json2[key]}")
+        else:
+            response.append(f"  {key}: {json1[key]}")
+
+    # print response formated
+    print("{")
+    for text in response:
+        print("  " + text)
+    print("}")
+
+
+def main():
+    parser = init_cli_parser()
     args = parser.parse_args()
-    print(args.accumulate(args))
-    # print args.
+    generate_diff(args.first_file, args.second_file)
 
 
 if __name__ == '__main__':
