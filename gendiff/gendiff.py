0199853b9ac91b0fef0e9d82cb3ee24122eca593
gleboss-redfab <gleb.v@redfab.ru>
2023-06-07 20:47:19 +0300

    implement yaml diff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,38 +1,57 @@
 import json
 
-# import yaml
-
-# yaml.load(open("data/file1.yaml"), Loader=yaml.BaseLoader)
+import yaml
 
 
-def generate_diff(path1: str, path2: str) -> str:
-    """Campare 2 json files and return formated diff
+def read_data(path1: str, path2: str) -> tuple:
+    # check extention and read data
+    ext1 = path1.split('.')[-1]
+    ext2 = path2.split('.')[-1]
+    if ext1 == "json":
+        data1 = json.load(open(path1))
+    elif ext1 in ["yaml", "yml"]:
+        data1 = yaml.load(open(path1), Loader=yaml.BaseLoader)
+    else:
+        print("Wrong path_1 extention!")
+        exit()
+
+    if ext2 == "json":
+        data2 = json.load(open(path2))
+    elif ext2 in ["yaml", "yml"]:
+        data2 = yaml.load(open(path2), Loader=yaml.BaseLoader)
+    else:
+        print("Wrong path_2 extention!")
+        exit()
+
+    return data1, data2
+
+
+def generate_diff(path1: str, path2: str) -> str | None:
+    """Campare 2 json or yaml files and return formated diff
     Args:
-        path1 (str): path to 1st JSON
-        path2 (str): path to 1st JSON
+        path1 (str): path to 1st file
+        path2 (str): path to 1st file
     Returns:
         str: formated diff
     """
-    # read data
-    json1 = json.load(open(path1))
-    json2 = json.load(open(path2))
+    data1, data2 = read_data(path1, path2)
 
     # get unique keys and sort it
-    keys = list(set(json1.keys()) | set(json2.keys()))
+    keys = list(set(data1.keys()) | set(data2.keys()))
     keys.sort()
 
     # compare and create response
     response = "{\n"
     for key in keys:
-        if key in json1.keys() and key not in json2.keys():
-            response += f"  - {key}: {json1[key]}\n"
-        elif key in json2.keys() and key not in json1.keys():
-            response += f"  + {key}: {json2[key]}\n"
-        elif json1[key] != json2[key]:
-            response += f"  - {key}: {json1[key]}\n"
-            response += f"  + {key}: {json2[key]}\n"
+        if key in data1.keys() and key not in data2.keys():
+            response += f"  - {key}: {str(data1[key]).lower()}\n"
+        elif key in data2.keys() and key not in data1.keys():
+            response += f"  + {key}: {str(data2[key]).lower()}\n"
+        elif data1[key] != data2[key]:
+            response += f"  - {key}: {str(data1[key]).lower()}\n"
+            response += f"  + {key}: {str(data2[key]).lower()}\n"
         else:
-            response += f"    {key}: {json1[key]}\n"
+            response += f"    {key}: {data1[key]}\n"
 
     response += "}"
     return response
